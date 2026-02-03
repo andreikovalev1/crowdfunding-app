7ddda03880bf902b00df3326814e5f54e1a667ca
andrewunreal <andreykovalev772@gmail.com>
2026-02-04 00:16:27 +0300

    fix: Added a new plugin to the config for copying the public folder to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -45,6 +46,17 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.png'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: "public", 
+          to: "",
+          globOptions: {
+            ignore: ["**/index.html"],
+          },
+        },
+      ],
     }),
   ],
   devServer: {
